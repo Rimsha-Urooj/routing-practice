f38eaa6458035dda11e53c216ecb388d56f5e84c
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-20 13:08:22 +0500

    adding dummy data and more content

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import QuoteForm from '../components/quotes/QuoteForm';
 
 function NewQuote() {
+    const addQuoteHandler = (quoteData) => {
+        console.log(quoteData);
+    };
     return (
-        <h1>
-            New Quote Page
-        </h1>
+       <QuoteForm onAddQuote = {addQuoteHandler} />
     );
 };
 
