b8c0633eca32562c0a5f250a541bf3775736ed62
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-20 15:37:56 +0500

    nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Fragment } from 'react';
-import { useParams, Route } from 'react-router-dom';
+import { useParams, Route, Link } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
 
@@ -20,7 +20,7 @@ const DUMMY_QUOTES = [
 function QuoteDetail() {
     const params = useParams();
     const quote = DUMMY_QUOTES.find((quote)=> quote.id===params.quoteId);
-    
+
     if (!quote){
         return <p>No Quote Found</p>
     }
@@ -28,6 +28,14 @@ function QuoteDetail() {
     return (
         <Fragment>
             <HighlightedQuote text={quote.text} author={quote.author} />
+            <Route path={`/quotes/${params.quoteId}`} exact>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`/quotes/${params.quoteId}/comments`}>
+                        Load Comments
+                    </Link>
+                </div>
+            </Route>
+            
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
